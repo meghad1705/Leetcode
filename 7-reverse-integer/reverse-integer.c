00fbff486b86a6444ef95c63a54dc406eaf7e77c
megha_doddamani <meghad1705@gmail.com>
2026-03-12 21:38:06 +0530

    Time: 6 ms (23.13%) | Memory: 9 MB (18.16%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,15 @@
+int reverse(int x) {
+    int rev = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+
+        if (rev > 214748364 || rev < -214748364)
+            return 0;
+
+        rev = rev * 10 + digit;
+        x = x / 10;
+    }
+
+    return rev;
+}
