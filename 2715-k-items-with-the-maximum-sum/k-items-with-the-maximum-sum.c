53f8861828551ab91493d47fea5b1582677908bd
megha_doddamani <meghad1705@gmail.com>
2026-03-12 15:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (6.45%) - LeetSync

---
+++ 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.c
@@ -0,0 +1,13 @@
+int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+    
+    if(numOnes>=k){
+        return k;
+    }
+    else if(k<=numOnes+numZeros){
+        return numOnes;
+    }
+    else{
+        return numOnes-(k-(numOnes+numZeros));
+    }
+    
+}
