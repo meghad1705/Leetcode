3873ffd22cb9a7a452f991ce166a941aecb7d2c5
megha_doddamani <meghad1705@gmail.com>
2026-03-12 22:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (15.49%) - LeetSync

--- 7-reverse-integer/reverse-integer.c
+++ 7-reverse-integer/reverse-integer.c
@@ -1,17 +1,11 @@
 int reverse(int x) {
-    int rev = 0;
-
+      long rev = 0;
     while (x != 0) {
-        int digit = x % 10;
-        x = x / 10;
-
-        if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
+        int pop = x % 10;
+        x /= 10;
+        rev = rev * 10 + pop;
+        if (rev > INT_MAX || rev < INT_MIN)
             return 0;
-        if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
-            return 0;
-
-        rev = rev * 10 + digit;
     }
-
-    return rev;
+    return (int)rev;
 }
