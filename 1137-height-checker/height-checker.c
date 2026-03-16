cc5bb6c53a8e24fa9e76d2a82545506aa0c3556f
megha_doddamani <meghad1705@gmail.com>
2026-03-16 22:58:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (11.38%) - LeetSync

--- 1137-height-checker/height-checker.c
+++ 1137-height-checker/height-checker.c
@@ -5,12 +5,10 @@ int heightChecker(int* heights, int heightsSize) {
 
     int expected[heightsSize];
 
-    // copy array
     for(int i = 0; i < heightsSize; i++){
         expected[i] = heights[i];
     }
 
-    // sort expected array (bubble sort)
     for(int i = 0; i < heightsSize - 1; i++){
         for(int j = 0; j < heightsSize - i - 1; j++){
             if(expected[j] > expected[j+1]){
@@ -21,7 +19,6 @@ int heightChecker(int* heights, int heightsSize) {
         }
     }
 
-    // count mismatches
     int count = 0;
 
     for(int i = 0; i < heightsSize; i++){
